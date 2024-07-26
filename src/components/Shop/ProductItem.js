d3b3e57b1e01fc8bba3c0d334402df67a182e11c
Ghazaleh Ansari <ghazaaleh.ansari@gmail.com>
2024-07-26 13:10:27 +0200

    backend is created and useEffect is used with Redux

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -10,6 +10,9 @@ const ProductItem = (props) => {
   const { title, price, description, id } = props;
 
   const addToCartHandler = () => {
+    // and then senf http request
+    // fetch('firebase-url',{method:'POST', body:JSON.stringify(newCart)})
+    
     dispatch(
       cartActions.addItemToCart({
         id,
